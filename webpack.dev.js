9cd9b0fd16f1fca9e6a0bbc7238f5f76ce89db00
Fiqri Syah Redha <wibulo27@gmail.com>
2020-11-10 13:41:30 +0700

    Add clear terminal webpack plugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,11 +2,13 @@
 const { merge } = require('webpack-merge');
 const path = require('path');
 const common = require('./webpack.common');
+const CleanTerminalPlugin = require('clean-terminal-webpack-plugin');
 
 module.exports = merge(common, {
 	mode: 'development',
 	devServer: {
 		contentBase: path.resolve(__dirname, 'dist'),
 		compress: false
-	}
+	},
+	plugins: [new CleanTerminalPlugin()]
 });
