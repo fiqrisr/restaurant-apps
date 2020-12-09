d322249bab9cec89101e81af69e8216a2b4da094
Fiqri Syah Redha <wibulo27@gmail.com>
2020-12-09 21:56:28 +0700

    Add bookmark one restaurant test case

--- e2e/Bookmark_Restaurants.spec.js
+++ e2e/Bookmark_Restaurants.spec.js
@@ -14,4 +14,8 @@ Scenario('Bookmark one restaurant', ({ I }) => {
 	I.see('Empty Bookmark', '.bg-text');
 	I.amOnPage('/');
 	I.seeElement({ shadow: ['rz-restaurant-list'] });
+	I.click({ shadow: ['rz-restaurant-list', 'rz-restaurant-card'] });
+	I.click({ shadow: ['rz-restaurant-header', 'div.bookmark'] });
+	I.amOnPage('/favorites');
+	I.seeElement({ shadow: ['rz-restaurant-list', 'rz-restaurant-card'] });
 });
