9cd2be167d5d2784fece496fbcc40214e4178def
Fiqri Syah Redha <wibulo27@gmail.com>
2020-12-11 02:42:49 +0700

    Add fallback image to hero image component

--- src/scripts/components/hero-image/rz-hero-image.ts
+++ src/scripts/components/hero-image/rz-hero-image.ts
@@ -19,12 +19,23 @@ export class rzHeroImage extends LitElement {
 	@property({ type: String, reflect: true })
 	height!: string;
 
+	@property({ type: String, reflect: true })
+	fallbackImage!: string;
+
 	static get styles(): CSSResultArray {
 		return [styles.shared, styles.heroImage];
 	}
 
 	render(): TemplateResult {
 		return html` <div class="hero-image">
+			${this.fallbackImage !== undefined
+				? html`<picture style="display:none;">
+						<source srcset="${this.image}" />
+						<source srcset="${this.fallbackImage}" />
+						<img src="${this.fallbackImage}" />
+				  </picture>`
+				: html``}
+
 			<div class="hero-text">
 				<slot name="title"></slot>
 				<slot name="tagline"></slot>
@@ -36,7 +47,22 @@ export class rzHeroImage extends LitElement {
 
 	firstUpdated(): void {
 		const heroImage = <HTMLElement>this.shadowRoot?.querySelector('.hero-image');
-		heroImage.style.backgroundImage = `var(--${this.gradient}),url("${this.image}")`;
 		heroImage.style.height = this.height;
+
+		if (this.fallbackImage !== undefined) {
+			const pictureEl = <HTMLElement>this.shadowRoot?.querySelector('picture');
+
+			if (pictureEl.getElementsByTagName('img')[0].src !== undefined) {
+				heroImage.style.backgroundImage = `var(--${
+					this.gradient
+				}),url("${pictureEl.getElementsByTagName('img')[0].src.toString()}")`;
+			} else {
+				heroImage.style.backgroundImage = `var(--${this.gradient}),url("${this.image}")`;
+			}
+		} else {
+			heroImage.style.backgroundImage = `var(--${this.gradient}),url("${this.image}")`;
+		}
+
+		this.requestUpdate();
 	}
 }
--- src/scripts/views/home-view/home-view.ts
+++ src/scripts/views/home-view/home-view.ts
@@ -5,7 +5,8 @@ import store from 'store';
 export class HomeView extends LitElement {
 	render(): TemplateResult {
 		return html` <rz-hero-image
-				image="../public/images/hero-image.jpg"
+				image="../public/images/hero-image.webp"
+				fallbackImage="../public/images/hero-image.jpg"
 				gradient="gradient-1"
 				height="500px"
 			>
